ad138a2cf19571e07ef9222ec3798b8419130ffa
Admin <iutroanne@gmail.com>
2025-11-12 09:51:23 +0100

    Base du serv web

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,48 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/fr/homefr.html.twig', [
             'controller_name' => 'HomeController',
+            'langue' => 'fr',
+        ]);
+    }
+
+    #[Route('/fr', name: 'app_frhome')]
+    public function homefr(): Response
+    {
+        return $this->render('home/fr/homefr.html.twig', [
+            'controller_name' => 'HomeController',
+            'langue' => 'fr',
+        ]);
+    }
+
+    #[Route('/base', name: 'app_frbase')]
+    public function basefr(): Response
+    {
+        return $this->render('home/fr/basefr.html.twig', [
+            'controller_name' => 'HomeController',
+            'langue' => 'fr',
+        ]);
+    }
+
+    #[Route('/en', name: 'app_enhome')]
+    public function homeen(): Response
+    {
+        return $this->render('home/en/homeen.html.twig', [
+            'controller_name' => 'HomeController',
+            'langue' => 'en',
+        ]);
+    }
+    #[Route('/en/base', name: 'app_enbase')]
+    public function baseen(): Response
+    {
+        return $this->render('home/en/baseen.html.twig', [
+            'controller_name' => 'HomeController',
+            'langue' => 'en',
         ]);
     }
 }
